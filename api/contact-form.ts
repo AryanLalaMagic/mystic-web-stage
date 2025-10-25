6330c6a527fc5ee576d86930080ee0aae28112f1
Ruchika Sreedhar <ruchikasreedhar29@gmail.com>
2025-10-25 20:45:00 +0530

    cors

--- api/contact-form.ts
+++ api/contact-form.ts
@@ -1,6 +1,11 @@
 import { neon } from '@neondatabase/serverless';
 import { Resend } from 'resend';
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+};
 
 interface ContactFormData {
   name: string;
